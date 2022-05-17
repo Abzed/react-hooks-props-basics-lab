756301c79f89563de85f0499d47b7698f1d436b0
Abzed <ibraabzed@gmail.com>
2022-05-17 17:14:10 +0300

    commit tree 90bb518d80d71021b63dbef659f7560bb12e0556

--- src/components/App.js
+++ src/components/App.js
@@ -10,9 +10,9 @@ console.log(user);
 function App() {
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar user={user} />
+      <Home name={user.name} color={user.color} city={user.city} />
+      <About bio={user.bio} linkedin={user.links.linkedin} github={user.links.github} /> 
     </div>
   );
 }
